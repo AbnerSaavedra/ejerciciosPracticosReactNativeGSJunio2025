1b8fb928a85e3a27fc3af24c834e289f6f525258
Abner Saavedra <ingenieroabnersaavedra@gmail.com>
2025-06-06 11:06:30 -0400

    Scroll view

--- index.ts
+++ index.ts
@@ -6,8 +6,9 @@ import LotsOfGreetings from './helloWorldProps';
 import Counter from './stateReactNative';
 import HelloCat from './helloCat';
 import PizzaTranslator from './manejoTextoEntrada';
+import Scroll from './scrollView';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(PizzaTranslator);
+registerRootComponent(Scroll);
---
+++ scrollView.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Image, ScrollView, Text, View} from'react-native';
+const logo = {
+uri: 'https://reactnative.dev/img/tiny_logo.png',
+width: 64,
+height: 64,
+};
+const Scroll = () => (
+<ScrollView>
+    <Text style={{fontSize: 96}}>Scrollme plz</Text>
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Text style={{fontSize: 96}}>Ifyoulike</Text>
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Text style={{fontSize: 96}}>Scrollingdown</Text>
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Text style={{fontSize: 96}}>What'sthebest</Text>
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Text style={{fontSize: 96}}>Frameworkaround?</Text>
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Text style={{fontSize: 80}}>ReactNative</Text>
+</ScrollView>
+);
+export default Scroll;
